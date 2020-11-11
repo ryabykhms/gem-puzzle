5cbf8974e61732a51c6ae9880404650ee5c602cd
Maxim Ryabykh <ryabykhms@gmail.com>
2020-11-11 12:53:25 +0300

    feat: implement moves counter and timer

--- gem-puzzle/src/js/index.js
+++ gem-puzzle/src/js/index.js
@@ -6,7 +6,6 @@ let boardState = {
 };
 
 const isSolutionExist = (size, arr) => {
-  const length = size ** 2;
   let inversion = 0;
   arr.forEach((item, i, array) => {
     if (item) {
@@ -45,7 +44,6 @@ const createDices = (size) => {
   const dices = [];
   const diceCount = size ** 2;
   const diceSize = Math.round(100 / size) - 2;
-  const emptyDiceIndex = diceCount - 1;
   const current = randomGeneration(size);
   current.forEach((item, i) => {
     const dice = document.createElement('div');
@@ -75,6 +73,18 @@ const size = 4;
 const body = document.querySelector('body');
 const screen = document.createElement('div');
 const panel = document.createElement('div');
+const time = document.createElement('div');
+time.classList.add('time');
+const timeValue = document.createElement('span');
+timeValue.classList.add('time__value');
+timeValue.textContent = 0;
+const moves = document.createElement('div');
+moves.classList.add('moves');
+const movesValue = document.createElement('span');
+movesValue.classList.add('moves__value');
+movesValue.textContent = 0;
+const options = document.createElement('div');
+options.classList.add('options');
 let board = document.createElement('board');
 const { dices, current } = createDices(size);
 screen.classList.add('screen');
@@ -92,6 +102,7 @@ const isEmptyNear = (order, emptyOrder, size) => {
   return isOrderAbove || isOrderBelow || isOrderLeft || isOrderRight;
 };
 
+let movesCount = 0;
 board.addEventListener('click', (e) => {
   const dice = e.target;
   if (
@@ -104,9 +115,31 @@ board.addEventListener('click', (e) => {
     if (isEmptyNear(diceOrder, emptyDiceOrder, size)) {
       dice.style.order = emptyDiceOrder;
       emptyDice.style.order = diceOrder;
+      movesCount++;
+      movesValue.textContent = movesCount;
     }
   }
 });
+moves.append('Moves: ');
+moves.append(movesValue);
+time.append('Time: ');
+time.append(timeValue);
+options.append('New Game');
+panel.append(time);
+panel.append(moves);
+panel.append(options);
 screen.append(panel);
 screen.append(board);
 body.prepend(screen);
+
+const timeInterval = setInterval(() => {
+  timeValue.textContent = +timeValue.textContent + 1;
+}, 1000);
+
+options.addEventListener('click', (e) => {
+  clearInterval(timeInterval);
+  timeValue.textContent = 0;
+  movesValue.textContent = 0;
+  const { dices, current } = createDices(size);
+  board.innerHtml = dices;
+});
