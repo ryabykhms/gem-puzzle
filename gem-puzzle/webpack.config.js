ef585334331a1b981c4a489087f5555f58fe952f
Maxim Ryabykh <ryabykhms@gmail.com>
2020-11-12 14:51:28 +0300

    feat: add favicon

--- gem-puzzle/webpack.config.js
+++ gem-puzzle/webpack.config.js
@@ -83,14 +83,14 @@ const plugins = () => {
       },
     }),
     new CleanWebpackPlugin(),
-    // new CopyWebpackPlugin({
-    //   patterns: [
-    //     {
-    //       from: path.resolve(__dirname, 'src/assets'),
-    //       to: path.resolve(__dirname, 'dist/assets'),
-    //     },
-    //   ],
-    // }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: path.resolve(__dirname, 'dist/assets'),
+        },
+      ],
+    }),
     new MiniCssExtractPlugin({
       filename: filename('css'),
     }),
